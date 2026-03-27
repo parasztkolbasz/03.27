2b43b67a5bd8943c47b839e3dbf0b4aa23051ce6
parasztkolbasz <epick.gamer6900@gmail.com>
2026-03-27 13:48:33 +0100

    Add files via upload

---
+++ 03.27/03.27/Kiadas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._27
+{
+    internal class Kiadas
+    {
+        public int ev;
+        public string orszag;
+        public string cim;
+        public int darab;
+        
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Orszag { get => orszag; set => orszag = value; }
+        public int Darab { get => darab; set => darab = value; }
+        public int Ev{ get => ev; set => ev = value; }
+
+
+        public Kiadas(string cim,string szerzo,int darab, int ev)
+        {
+            this.ev = ev;
+            this.orszag = orszag;
+            this.cim = cim;
+            this.darab = darab;
+           
+        } 
+        
+
+    }
+}
---
+++ 03.27/03.27/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kiadas> kiadasok;
+            
+            StreamReader sr = new StreamReader("kiadas.txt");
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(";");
+                foreach (var item in sor)
+                {
+                    
+                }
+            }
+            sr.Close();
+        }
+    }
+}
